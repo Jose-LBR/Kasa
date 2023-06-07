86022cf1ddb92175d214e072a5dcb45cb43f40db
José Le Ber <jose.leber.dev@gmail.com>
2023-06-07 18:20:19 +0200

    first draft of the Header

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="header-container d-flex justify-content-space-between">
+      <div class="logo">Kasa</div>
+      <nav class="b1">
+        <router-link to="home" class="nav1">Accueil</router-link>
+        <router-link to="about" lass="nav2">A Propos</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  width: 1240px;
+  height: 68px;
+  position: absolute;
+  left: 100px;
+  top: 40px;
+
+  .logo {
+    width: 210.32px;
+    height: 68px;
+  }
+
+  nav {
+    width: 309px;
+    height: 34px;
+    position: relative;
+
+    .nav1 {
+      color: #ff6060;
+      position: absolute;
+      //   left: 75.08%;
+      right: 13, 95%;
+      top: 25%;
+      bottom: 25%;
+    }
+  }
+}
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="notfound">404</div>
+</template>
+
+<style scoped>
+.notfound {
+  width: 597px;
+  height: 263;
+}
+</style>
